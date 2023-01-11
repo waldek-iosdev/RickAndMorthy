38a889ae14ea73587989ddd15e70a73bf23ae310
Waldek <waldek@flexisoft.pl>
2023-01-11 17:29:22 +0100

    Created API request

--- RickAndMorthy/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorthy/Controllers/Core/RMCharacterViewController.swift
@@ -17,7 +17,16 @@ final class RMCharacterViewController: UIViewController {
         view.backgroundColor = .systemBackground
         title = "Characters"
     
-        // Do any additional setup after loading the view.
+        let request = RMRequest(endpoint: .character, queryParameters: [
+            URLQueryItem(name: "name", value: "rick"),
+            URLQueryItem(name: "status", value: "alive")
+        ]
+        )
+        print(request.url)
+        
+        RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+        
+        }
     }
 
 }
